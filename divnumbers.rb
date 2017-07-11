1aed7bac01f9041d032f3f5cab2d2b57f3bae050
ANITA KING <ADKING>
2017-07-11 14:36:15 -0400

    Expanded div 'MinedMinds'

--- divnumbers.rb
+++ divnumbers.rb
@@ -1,5 +1,9 @@
  1.upto(100) do |i|
-      if i % 3 == 0
+      if i % 5 == 0 and i % 3 == 0
+        puts "MinedMines"
+      elsif i % 5 == 0
+        puts "Minds"
+      elsif i % 3 == 0
         puts "Mined"
       else
         puts i

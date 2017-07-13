7052395c1650b45049492bf6204f9f468c5597c6
ANITA KING <ADKING>
2017-07-13 13:41:11 -0400

    successful test of all scenarios

--- minedmindsarray.rb
+++ minedmindsarray.rb
@@ -1,10 +1,18 @@
 
 def create_mined_minds_array()
-	array1 = []
 	counter = 1
+	array1 = []
 	100.times do
-		array1.push(counter) 
-		counter = counter + 1
+		if counter % 5 == 0 and counter % 3 == 0
+			array1.push("MinedMinds")
+		elsif counter % 5 == 0
+			array1.push("Minds") 
+		elsif counter % 3 == 0
+			array1.push("Mined")
+		else 
+			array1.push(counter)
+      	end
+      	counter = counter + 1
 	end
 	array1
 end
--- minedmindsarray_test.rb
+++ minedmindsarray_test.rb
@@ -17,4 +17,29 @@ class TestMinedMindsArray < Minitest::Test #camelcase with class name otherwise 
 		p mm_array
 		assert_equal(100, mm_array.length)
 	end
+
+	def test_zero_position_in_array_is_one
+		mm_array = create_mined_minds_array()
+		assert_equal(1, mm_array[0])
+	end
+
+	def test_second_position_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("Mined", mm_array[2])
+	end
+
+	def test_fifth_position_in_array_is_mined
+		mm_array = create_mined_minds_array()
+		assert_equal("Mined", mm_array[5])
+	end
+	
+	def test_fourth_position_in_array_is_mined_minds
+		mm_array = create_mined_minds_array()
+		assert_equal("Minds", mm_array[4])
+	end
+
+	def test_fourteenth_position_in_array_is_mined_minds
+		mm_array = create_mined_minds_array()
+		assert_equal("MinedMinds", mm_array[14])
+	end
 end
